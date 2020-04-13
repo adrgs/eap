941965939670e84f0e628014f7587dfe1cb2f6fa
adrgs <albastroiudragos@gmail.com>
2020-04-13 15:37:23 +0300

    Citit din fisier

---
+++ Laborator08/src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        try {
+//            DataOutputStream dataOutputStream = null;
+//            Scanner scanner = new Scanner(System.in);
+//
+//            System.out.println("Numele fisierului este:");
+//            String fileName = scanner.nextLine();
+//            dataOutputStream = new DataOutputStream(new FileOutputStream(fileName));
+//
+//            System.out.println("Nr de elemente: ");
+//            int n = scanner.nextInt();
+//            dataOutputStream.writeInt(n);
+//
+//            System.out.println("Elementele sunt:");
+//            for (int i=0;i<n;i++)
+//            {
+//                dataOutputStream.writeDouble(scanner.nextDouble());
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        try (DataInputStream dataInputStream = new DataInputStream(new FileInputStream("file1.txt"))){
+//
+//            int n = dataInputStream.readInt();
+//            System.out.println("n = " + n);
+//
+//            System.out.println("Elementele sunt:");
+////            for (int i=0;i<n;i++) {
+////                System.out.print(dataInputStream.readDouble() + " ");
+////            }
+//            while (dataInputStream.available() >= 8)
+//            {
+//                System.out.print(dataInputStream.readDouble() + " ");
+//            }
+//            System.out.println();
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
